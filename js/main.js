a4bb472cd8ad126643e065e1093892a80c9d4281
Julius Alexander IV <alexaj5rpi@gmail.com>
2015-04-27 03:01:03 -0400

    Pivot. Using PeerJS

--- js/main.js
+++ js/main.js
@@ -1,18 +1,40 @@
-$(document).ready(function() {
+var apiKey = "ve6ha79sg9l766r";
+var pollees = {};
 
-  $("#new-poll").click(function() {
-    // alert("boobs");
-    $(".route").text(genRandRoute());
-  });
+$(document).ready(function() {
+  pollID = location.hash.substring(1);
+  console.log("Anchor: " + pollID);
+
+  if (!pollID) {
+    $("#new-poll").click(function() {
+      poll = newPoll();
+    });    
+  }
+  else {
+    pollee = connectToPoll(pollID);
+  }
 
 });
 
-function genRandRoute()
-{
-  var route = "";
-  var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+function connectToPoll(pollID) {
+  var pollee = new Peer({key: apiKey});
+  pollee.on("open", function(id) {
+    console.log("New pollee created with ID " + id);
+  });
 
-  for( var i=0; i < 6; i++ )
-      route += possible.charAt(Math.floor(Math.random() * possible.length));
-  return route;
+  pollee.connect(pollID);
+  return pollee;
+}
+
+function newPoll() {
+  var poll = new Peer({key: apiKey});
+  poll.on("open", function(id) {
+    console.log("Peer/poll id: " + id);
+    $(".route").text(id);
+  });
+
+  poll.on("connection", function(conn) {
+    console.log("New pollee: " + conn.peer);
+  });
+  return poll;
 }
